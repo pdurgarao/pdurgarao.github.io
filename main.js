260770f129aed8e45218facee97a932af3979933
saipolisetti <saipolisetti700@gmail.com>
2019-12-30 09:39:13 +0530

    add js file
    popups

---
+++ main.js
@@ -0,0 +1,15 @@
+
+    function change() {
+        document.getElementById("bar-progress").style.width="70%"
+    }
+    function initial() {
+        document.getElementById("bar-progress").style.width="30%"
+        
+
+    }
+    function initial1() {
+        document.getElementById("bar-progress").style.width="30%"
+        alert("successfully ordered")
+        
+
+    }

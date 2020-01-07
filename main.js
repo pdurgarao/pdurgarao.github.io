f1cfa69fd3e5085d11cad6c5b513e925f31158af
saipolisetti <saipolisetti700@gmail.com>
2020-01-07 06:47:56 +0530

    small changes
    like padding , data interval like that

--- main.js
+++ main.js
@@ -4,8 +4,6 @@
     }
     function initial() {
         document.getElementById("bar-progress").style.width="30%"
-        
-
     }
     function initial1() {
         document.getElementById("bar-progress").style.width="30%"
@@ -13,3 +11,58 @@
         
 
     }
+      function change1() {
+          document.getElementById("bar-progress1").style.width="70%"
+      }
+     function initial11() {
+         document.getElementById("bar-progress1").style.width="30%"
+     }
+
+     function change2() {
+        document.getElementById("bar-progress2").style.width="70%"
+    }
+   function initial21() {
+       document.getElementById("bar-progress2").style.width="30%"
+   }
+
+   function change3() {
+    document.getElementById("bar-progress3").style.width="70%"
+}
+function initial31() {
+   document.getElementById("bar-progress3").style.width="30%"
+}
+
+function change4() {
+    document.getElementById("bar-progress4").style.width="70%"
+}
+function initial41() {
+   document.getElementById("bar-progress4").style.width="30%"
+}
+
+function change5() {
+    document.getElementById("bar-progress5").style.width="70%"
+}
+function initial51() {
+   document.getElementById("bar-progress5").style.width="30%"
+}
+
+function changet() {
+    document.getElementById("bar-progress-today").style.width="70%"
+}
+function initialtoday() {
+   document.getElementById("bar-progress-today").style.width="30%"
+}
+
+function changet1() {
+    document.getElementById("bar-progress-today1").style.width="70%"
+}
+function initialtoday1() {
+   document.getElementById("bar-progress-today1").style.width="30%"
+}
+function changet2() {
+    document.getElementById("bar-progress-today2").style.width="70%"
+}
+function initialtoday2() {
+   document.getElementById("bar-progress-today2").style.width="30%"
+}
+
